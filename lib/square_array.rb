433b0075edef7608a8e6398ee2ccf26022e33e6b
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-01 23:28:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,19 @@
 def square_array(array)
-  # your code here
+  counter = 0
+
+  while square_array[counter]
+    puts square_array.square_array
+    counter += 1
+  end
 end
+
+
+
+#def output_array_elements(array)
+ # counter = 0
+ #
+  #while array[counter] do
+   # puts array[counter]
+    #counter += 1
+  #end
+#end
